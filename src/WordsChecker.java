b4ce1990dac5551cf815b962f2a30ac3e81f8a4e
VovaZ <zalikvova1998@gmail.com>
2023-04-26 10:48:11 +0300

    Измение конструктора

--- src/WordsChecker.java
+++ src/WordsChecker.java
@@ -2,16 +2,16 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class WordsChecker {
-    protected String TEXT;
-    public WordsChecker(String TEXT) {
-        this.TEXT = TEXT;
-    }
-    public boolean hasWord (String word) {
-        String[] words = TEXT.split("\\P{IsAlphabetic}+");
-        Set<String> set = new HashSet<>();
+    protected String text;
+     protected Set<String> set = new HashSet<>();
+    public WordsChecker(String text) {
+        this.text = text;
+        String[] words = text.split("\\P{IsAlphabetic}+");
         for (int i = 0; i < words.length; i++) {
             set.add(words[i]);
         }
+    }
+    public boolean hasWord (String word) {
         if (set.contains(word)) return true;
         else return false;
     }
