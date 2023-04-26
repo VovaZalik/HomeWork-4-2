c640e29c574f52a122a8c8be76f1e5b39d19ef38
VovaZ <zalikvova1998@gmail.com>
2023-04-26 14:10:42 +0300

    -text

--- src/WordsChecker.java
+++ src/WordsChecker.java
@@ -2,17 +2,14 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class WordsChecker {
-    protected String text;
-     protected Set<String> set = new HashSet<>();
+    protected Set<String> set = new HashSet<>();
     public WordsChecker(String text) {
-        this.text = text;
         String[] words = text.split("\\P{IsAlphabetic}+");
         for (int i = 0; i < words.length; i++) {
             set.add(words[i]);
         }
     }
     public boolean hasWord (String word) {
-        if (set.contains(word)) return true;
-        else return false;
+        return set.contains(word);
     }
 }
